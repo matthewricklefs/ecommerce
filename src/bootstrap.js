c80efb8f0056e34393a62e300097bbef7fe6859c
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-20 20:36:05 -0500

    set up redux form

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Router, Switch, Route } from "react-router-dom";
 import reducers from "./reducers";
 
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(
+  compose(window.devToolsExtension ? window.devToolsExtension() : (f) => f)(
+    createStore
+  )
+);
 
 import "./style/main.scss";
-import history from './history';
+
+import history from "./history";
 import Layout from "./components/layout";
-import SignIn from './components/auth/signin';
-import SignUp from './components/auth/signup'
+import SignIn from "./components/auth/signin";
+import SignUp from "./components/auth/signup";
 
 function main() {
   ReactDOM.render(
@@ -20,7 +24,9 @@ function main() {
       <Router history={history}>
         <Layout>
           <Switch>
-            <Route path="/" exact component={ SignIn }></Route>
+            <Route path="/" exact component={SignIn}></Route>
+            <Route path="/signin" exact component={SignIn}></Route>
+            <Route path="/signup" exact component={SignUp}></Route>
           </Switch>
         </Layout>
       </Router>
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { reduxForm, Field} from "redux-form";
 
 class SignIn extends Component {
   render() {
-    return (
-      <div className="sign-in">
-        Sign In
-      </div>
-    )
+    return <div className="sign-in">Sign In</div>;
   }
 }
 
-export default SignIn
+SignIn = reduxForm({
+  form: "SignIn",
+})(SignIn);
+
+export default SignIn;
