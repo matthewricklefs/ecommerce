9c4514a1819e44b7d44e8f11d57adf03306c09ce
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-20 21:12:26 -0500

    builds form button component in formFields.js and adds submit/login button to sign into the form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -13,9 +13,26 @@ export class FormInput extends Component {
           type={type}
           placeholder={placeholder}
           {...input}
+        ></input>
+      </div>
+    );
+  }
+}
+
+export class FormButton extends Component {
+  render() {
+    const { className, title, type, onClick, input } = this.props;
+
+    return (
+      <div className={` ${className} form-button `}>
+        <button
+          className={` form-button__button `}
+          type={type}
+          {...input}
+          onClick={onclick}
         >
-          
-        </input>
+          {title}
+        </button>
       </div>
     );
   }
